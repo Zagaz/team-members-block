e9eb6305e5cca2ccd607355e06e1f52f2ad71adb
Zagaz <andre_ranulfo@hotmail.com>
2024-03-10 02:58:45 -0300

    🧪 - Sass

--- build/team-members/index.asset.php
+++ build/team-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bec070fb946be04b7474');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'dbbccacc00164c12b479');
