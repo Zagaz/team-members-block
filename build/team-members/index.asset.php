663ae0efb78b4cfc4b76e15e529101abb8aa8f9c
Zagaz <andre_ranulfo@hotmail.com>
2024-03-13 13:38:28 -0300

    💎TMS/BE=> block-list-appender

--- build/team-members/index.asset.php
+++ build/team-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'da13539b261610131dcc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c3b5d02da41359ef6689');
