126a7c5639db8ec704f21b5fe29d94d4cf735680
Zagaz <andre_ranulfo@hotmail.com>
2024-03-13 13:42:47 -0300

    ☕

--- build/team-members/index.asset.php
+++ build/team-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c3b5d02da41359ef6689');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c3d92b3bda53753a4578');
