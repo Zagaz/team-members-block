9f998eb2898470dac07788b8a130fc7ac5bbf83a
Zagaz <andre_ranulfo@hotmail.com>
2024-03-22 23:25:29 -0300

    🐞 Update team member layout styles (Backend)

--- build/team-members/index.asset.php
+++ build/team-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6188cb579dde1dcdd43d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '976224b3630a06244911');
